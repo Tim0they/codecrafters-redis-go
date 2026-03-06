c9e4f0780c70fa08c4c8fbec677297015914a075
Lufuno <ltshishivhiri@gmail.com>
2026-03-07 00:44:31 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -18,11 +18,13 @@ func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
+		fmt.Printf("%v", err)
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	conn.Write([]byte("+PONG\r\n"))
 }
